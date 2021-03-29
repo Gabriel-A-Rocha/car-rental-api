6f7c648138c272f72ebf28212241cd04a6c98013
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-03-28 21:01:34 -0400

    feat: install typescript and ts-node-dev

--- index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-
-const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ msg: "hello world with TypeScript" });
-});
-
-app.listen(3333);
