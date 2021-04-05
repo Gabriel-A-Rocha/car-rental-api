3b0ab628e1b120af9cab2fc12d4025e5f321950b
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-04 20:16:32 -0400

    refactor: group routes in a single router object

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from "express";
-import { categoriesRouter } from "./routes/categories.routes";
-import { specificationsRouter } from "./routes/specification.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/categories", categoriesRouter);
-app.use("/specifications", specificationsRouter);
+app.use(router);
 
 const port = 3333;
 
