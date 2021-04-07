c247f56d7191dbcb1789f8ae976f7cd4be6e406b
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-07 19:48:58 -0400

    feat: create tmp folder to store upload files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
+import multer from "multer";
+
+const upload = multer({ dest: "./tmp" });
+
 const categoriesRouter = Router();
 
 categoriesRouter
@@ -10,5 +14,14 @@ categoriesRouter
   .post((req, res) => createCategoryController.handle(req, res))
 
   .get((req, res) => listCategoriesController.handle(req, res));
+
+categoriesRouter
+  .route("/import")
+
+  .post(upload.single("file"), (req, res) => {
+    console.log(req.file);
+    console.log(req.body);
+    console.log(req.body);
+  });
 
 export { categoriesRouter };
