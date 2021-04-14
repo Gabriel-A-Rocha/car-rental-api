d6e9ffdb6294b72dea9f98ade5e01db0d7e71851
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-13 23:02:26 -0400

    feat: set up swagger route documentation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { categoriesRouter } from "./categories.routes";
 import { specificationsRouter } from "./specification.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from "../swagger.json";
 
 const router = Router();
 
 router.use("/categories", categoriesRouter);
 router.use("/specifications", specificationsRouter);
+
+// documentation routes
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerConfig));
 
 export { router };
